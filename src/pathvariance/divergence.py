c8a8053b89469cad05fbf846a4ac527e411e8e29
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2016-08-11 12:05:00 -0800

    fix: correct path-analysis

--- src/pathvariance/divergence.py
+++ src/pathvariance/divergence.py
@@ -8,3 +8,13 @@ Once the modal path is known, two further questions matter for debugging:
 - How much do runs agree at each step? The per-step agreement rate is the
   fraction of runs whose token at that index equals the modal token, computed
   only over runs that are long enough to have a token there.
+
+The first divergence index is the smallest step index at which at least one run
+disagrees with the modal path, counting a run that has ended as a disagreement
+once the modal path still has tokens. It is ``None`` when every run matches the
+modal path exactly, which is the fully stable case.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
