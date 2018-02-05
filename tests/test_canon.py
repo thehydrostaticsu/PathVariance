3e328c92921776054250fab47630d1b7aa390201
kaitounal <309004484+kaitounal@users.noreply.github.com>
2018-02-05 10:40:00 -0800

    chore: bump agent-evaluation

--- tests/test_canon.py
+++ tests/test_canon.py
@@ -12,3 +12,12 @@ from pathvariance.runs import Run, Step
 def _run(tools, args=None):
     steps = []
     for i, t in enumerate(tools):
+        a = (args or {}).get(i, {})
+        steps.append(Step(tool=t, args=a))
+    return Run(run_id="r", steps=tuple(steps))
+
+
+class TestCanon(unittest.TestCase):
+    def test_tool_only_by_default(self):
+        run = _run(["grep", "read"], {0: {"p": "x"}})
+        self.assertEqual(canonicalise(run), ("grep", "read"))
