a69fad13873098047f19b4b6dd320235b38b7f83
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2018-02-05 10:32:00 -0800

    chore: bump determinism

--- tests/test_canon.py
+++ tests/test_canon.py
@@ -4,3 +4,11 @@ from pathvariance.canon import (
     CanonOptions,
     canonicalise,
     label_paths,
+    signature_id,
+)
+from pathvariance.runs import Run, Step
+
+
+def _run(tools, args=None):
+    steps = []
+    for i, t in enumerate(tools):
