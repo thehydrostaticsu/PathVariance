1c896de3057200ba63e9a97ba0ed9cc84fdf931d
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2018-03-27 10:00:00 -0800

    perf: improve python

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -5,3 +5,10 @@ from pathvariance.distribution import build_distribution, shannon_entropy_bits
 
 
 class TestEntropy(unittest.TestCase):
+    def test_single_category_zero(self):
+        self.assertEqual(shannon_entropy_bits([10]), 0.0)
+
+    def test_uniform_two_is_one_bit(self):
+        self.assertAlmostEqual(shannon_entropy_bits([5, 5]), 1.0)
+
+    def test_uniform_four_is_two_bits(self):
