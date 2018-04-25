42167e43493a2d42d9c4c03f7e2ba03bcafabdea
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2018-04-25 11:12:00 -0800

    fix: correct entropy

--- tests/test_divergence.py
+++ tests/test_divergence.py
@@ -3,3 +3,10 @@ import unittest
 from pathvariance.divergence import analyse_divergence
 
 
+class TestDivergence(unittest.TestCase):
+    def test_no_divergence_when_all_match(self):
+        modal = ("a", "b", "c")
+        sigs = [modal, modal, modal]
+        rep = analyse_divergence(sigs, modal)
+        self.assertIsNone(rep.first_divergence_index)
+        self.assertFalse(rep.diverges)
