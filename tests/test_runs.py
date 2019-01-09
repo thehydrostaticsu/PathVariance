7ca890bcb610f9360f2dca84366b27e1ce72b461
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2019-01-09 09:30:00 -0800

    perf: improve entropy

--- tests/test_runs.py
+++ tests/test_runs.py
@@ -5,3 +5,11 @@ from pathvariance.runs import ParseError, Step, parse_export
 
 
 class TestParseExport(unittest.TestCase):
+    def test_object_shape(self):
+        data = {
+            "task": "t",
+            "runs": [{"run_id": "r1", "steps": [{"tool": "a"}]}],
+        }
+        export = parse_export(data)
+        self.assertEqual(export.task, "t")
+        self.assertEqual(len(export), 1)
