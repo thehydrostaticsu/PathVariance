5036537b758ce04ea2c3329d5edad49ec645a943
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2019-02-06 10:15:00 -0800

    feat: add reproducibility

--- tests/test_stability.py
+++ tests/test_stability.py
@@ -7,3 +7,9 @@ from pathvariance.stability import Verdict, assess
 def _dist(a, b=0, c=0):
     sigs = [("a",)] * a + [("b",)] * b + [("c",)] * c
     return build_distribution(sigs)
+
+
+class TestStability(unittest.TestCase):
+    def test_under_min_refuses(self):
+        result = assess(_dist(2, 1), min_runs=5)
+        self.assertIs(result.verdict, Verdict.UNDER_MIN)
