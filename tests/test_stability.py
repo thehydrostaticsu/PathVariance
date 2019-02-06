5b727c83d7afa82db9616122f30f60eab8d61711
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2019-02-06 10:12:00 -0800

    fix: python

--- tests/test_stability.py
+++ tests/test_stability.py
@@ -2,3 +2,8 @@ import unittest
 
 from pathvariance.distribution import build_distribution
 from pathvariance.stability import Verdict, assess
+
+
+def _dist(a, b=0, c=0):
+    sigs = [("a",)] * a + [("b",)] * b + [("c",)] * c
+    return build_distribution(sigs)
