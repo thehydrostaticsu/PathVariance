9dbd13a2ceea584b08d34f0dbc2deb57edc08da6
Feldspar Labs <11375221+thehydrostaticsu@users.noreply.github.com>
2019-02-07 18:12:00 -0800

    test: cover agent-evaluation

--- tests/test_stability.py
+++ tests/test_stability.py
@@ -13,3 +13,8 @@ class TestStability(unittest.TestCase):
     def test_under_min_refuses(self):
         result = assess(_dist(2, 1), min_runs=5)
         self.assertIs(result.verdict, Verdict.UNDER_MIN)
+        self.assertIsNone(result.modal_share)
+        self.assertFalse(result.is_reportable)
+        self.assertIn("below the minimum", result.reason)
+
+    def test_stable_above_threshold(self):
